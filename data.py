93407b444c11885b9a790eff78ef39d3ed09322e
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-22 06:43:51 +0000

    Add Data class

---
+++ data.py
@@ -0,0 +1,35 @@
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+# Sheety configs
+SHEETY_ENDPOINT = os.environ.get("SHEETY_ENDPOINT")
+SHEETY_HEADERS = {
+    "Authorization": os.environ.get('SHEETY_AUTH')
+}
+
+
+class LookUpData:
+
+    def __init__(self):
+        self.data = {}
+
+    def get_data(self):
+        """This function get the data from the google sheets using sheety"""
+        sheety_response = requests.get(SHEETY_ENDPOINT, headers=SHEETY_HEADERS)
+        data = sheety_response.json()
+        self.data = data['prices']
+        return self.data
+
+    def update_iate_code(self):
+        """This add the city's IATA Code"""
+        for city in self.data:
+            update_code = {
+                "price": {
+                    "iataCode": city['iataCode']
+                }
+            }
+            PUT_ENDPOINT = f"{SHEETY_ENDPOINT}/{city['id']}"
+            response = requests.put(PUT_ENDPOINT, json=update_code, headers=SHEETY_HEADERS)
+            print(response.text)
