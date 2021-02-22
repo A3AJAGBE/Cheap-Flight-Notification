b43b33f29ae17af30ff3896db414d895f6da2ae1
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-22 09:25:38 +0000

    Add check flights function

--- search.py
+++ search.py
@@ -1,10 +1,13 @@
 import requests
 import os
+from flight_info import FlightInfo
 from dotenv import load_dotenv
+
 load_dotenv()
 
 TEQUILA_URL = os.environ.get("KIWI_TEQUILA_URL")
 TEQUILA_ENDPOINT = f"{TEQUILA_URL}/locations/query"
+TEQUILA_SEARCH_ENDPOINT = f"{TEQUILA_URL}/v2/search"
 HEADERS = {
     "apikey": os.environ.get("KIWI_TEQUILA_API")
 }
@@ -23,3 +26,37 @@ class Search:
         for d in data:
             code = d['code']
             return code
+
+    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
+        """This function checks for flight"""
+        PARAMETERS = {
+            "fly_from": origin_city_code,
+            "fly_to": destination_city_code,
+            "date_from": from_time,
+            "date_to": to_time,
+            "nights_in_dst_from": 5,
+            "nights_in_dst_to": 28,
+            "flight_type": "round",
+            "one_for_city": 1,
+            "max_stopovers": 0,
+            "curr": "EUR"
+        }
+
+        response = requests.get(TEQUILA_SEARCH_ENDPOINT, headers=HEADERS, params=PARAMETERS)
+        try:
+            data = response.json()['data'][0]
+        except IndexError:
+            print(f"No flights found for {destination_city_code}.")
+            return None
+
+        flight_info = FlightInfo(
+            price=data["price"],
+            origin_city=data["route"][0]["cityFrom"],
+            origin_airport=data["route"][0]["flyFrom"],
+            destination_city=data["route"][0]["cityTo"],
+            destination_airport=data["route"][0]["flyTo"],
+            out_date=data["route"][0]["local_departure"].split("T")[0],
+            return_date=data["route"][1]["local_departure"].split("T")[0]
+        )
+        print(f"{flight_info.destination_city}: €{flight_info.price}")
+        return flight_info
