609e3f4ff253f61537aa05051f18cf02327d79b9
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-22 10:04:23 +0000

    Add notification class

---
+++ notifications.py
@@ -0,0 +1,39 @@
+import os
+from twilio.rest import Client
+import smtplib
+from dotenv import load_dotenv
+load_dotenv()
+
+# Twilio configs
+ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID']
+AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
+
+# Email config
+EMAIL = os.environ.get('YMAIL')
+PASSWORD = os.environ.get('YMAIL_PASS')
+RECEIVER_EMAIL = os.environ.get('GMAIL')
+
+
+class Notification:
+
+    def __init__(self):
+        self.client = Client(ACCOUNT_SID, AUTH_TOKEN)
+
+    def send_sms(self, message):
+        """This function sends an SMS"""
+        message = self.client.messages.create(
+            body=f"{message} - A3AJAGBE LOW FLIGHT FINDER",
+            from_=os.environ.get('TWILIO_NUMBER'),
+            to=os.environ.get('MY_NUMBER'),
+        )
+        # Prints if successfully sent.
+        print(message.sid)
+
+    def send_email(self, message):
+        """This function sends an email"""
+        with smtplib.SMTP("smtp.mail.yahoo.com") as conn:
+            conn.starttls()
+            conn.login(user=EMAIL, password=PASSWORD)
+            conn.sendmail(from_addr=EMAIL,
+                          to_addrs=RECEIVER_EMAIL,
+                          msg=f"Subject:Low flight price Notification!!!\n\n{message}")
