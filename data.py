566e15d4d1677364ae5e43d5f146a88f83335886
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-23 06:08:01 +0000

    Add get members emails function

--- data.py
+++ data.py
@@ -5,6 +5,7 @@ load_dotenv()
 
 # Sheety configs
 SHEETY_ENDPOINT = os.environ.get("SHEETY_ENDPOINT")
+SHEETY_MEMBER_ENDPOINT = os.environ.get("SHEETY_MEMBER_ENDPOINT")
 SHEETY_HEADERS = {
     "Authorization": os.environ.get('SHEETY_AUTH')
 }
@@ -14,6 +15,7 @@ class LookUpData:
 
     def __init__(self):
         self.data = {}
+        self.member_data = {}
 
     def get_data(self):
         """This function get the data from the google sheets using sheety"""
@@ -33,3 +35,10 @@ class LookUpData:
             PUT_ENDPOINT = f"{SHEETY_ENDPOINT}/{city['id']}"
             response = requests.put(PUT_ENDPOINT, json=update_code, headers=SHEETY_HEADERS)
             print(response.text)
+
+    def get_member_emails(self):
+        """This function gets the member emails"""
+        response = requests.get(SHEETY_MEMBER_ENDPOINT, headers=SHEETY_HEADERS)
+        data = response.json()
+        self.member_data = data["members"]
+        return self.member_data
