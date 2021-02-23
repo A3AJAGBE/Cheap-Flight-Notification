e6e41dd7b2746b08aa8d282d92276007bbd566f8
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-23 08:39:09 +0000

    Refactor code

--- main.py
+++ main.py
@@ -12,14 +12,12 @@ notification = Notification()
 # Get the lookup data
 sheety_data = lookup_data.get_data()
 
-
 # Check if IATA Code column is empty
 if sheety_data[0]['iataCode'] == '':
     for row in sheety_data:
         row['iataCode'] = search.get_code(row['city'])
     lookup_data.data = sheety_data
     lookup_data.update_iata_code()
-
 
 # Get tomorrow's date and the date in 6 months time
 tomorrow = datetime.now() + timedelta(days=1)
@@ -38,20 +36,25 @@ for destination in sheety_data:
     )
 
     if flight is None:
+        print(f"No flights found.")
         continue
 
     if flight.price < destination["lowestPrice"]:
-        # notification.send_email(
-        #     message=f"Low price alert! "
-        #             f"Only {flight.price}(EUR) to fly from {flight.origin_city}-{flight.origin_airport} to "
-        #             f"{flight.destination_city}-{flight.destination_airport}, "
-        #             f"from {flight.out_date} to {flight.return_date}. "
-        #             f"From: A3AJAGBE LOW FLIGHTY"
-        # )
-        # notification.send_sms(
-        #     message=f"Low price alert! "
-        #             f"Only €{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to "
-        #             f"{flight.destination_city}-{flight.destination_airport}, "
-        #             f"from {flight.out_date} to {flight.return_date}. "
-        # )
-        print(f"{flight.price} for {flight.destination_city}")
+
+        members = lookup_data.get_member_emails()
+        emails = [member["email"] for member in members]
+        first_names = [member["firstName"] for member in members]
+
+        message = f"A3AJAGBE LOW FLIGHT NOTIFICATION\n" \
+                  f"Only €{flight.price} to fly from {flight.origin_city} ({flight.origin_airport} Airport)" \
+                  f" to {flight.destination_city} ({flight.destination_airport} Airport), " \
+                  f"from {flight.out_date} to {flight.return_date}."
+
+        if flight.stop_overs > 0:
+            message += f"\nThere's a stop over for this flight, via {flight.via_city}."
+
+        google_link = f"https://www.google.com/flights?hl=en#flt={flight.origin_airport}.{flight.destination_airport}.{flight.out_date}*{flight.destination_airport}.{flight.origin_airport}.{flight.return_date} "
+        notification.send_email(message, emails, google_link)
+    else:
+        print(f"Flight price to {flight.destination_city} higher than target price.")
+
